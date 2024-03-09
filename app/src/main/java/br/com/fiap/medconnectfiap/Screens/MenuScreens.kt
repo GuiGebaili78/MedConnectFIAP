122c7c2952664a6ef272aa4cd7e6a47f5cc0773d
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2024-03-09 14:36:32 -0300

    tela menu finalizada

--- app/src/main/java/br/com/fiap/medconnectfiap/Screens/MenuScreens.kt
+++ app/src/main/java/br/com/fiap/medconnectfiap/Screens/MenuScreens.kt
@@ -1,15 +1,7 @@
 package br.com.fiap.medconnectfiap.Screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,75 +51,75 @@ fun MenuScreen() {
                 Modifier.padding(10.dp)
             )
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        // Adiciona os botões de navegação
-        Column (modifier = Modifier
-            .align(alignment = Alignment.CenterHorizontally)
 
-        ){
-            Row (modifier = Modifier.background(Color.Transparent)){
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(AzulMedio),
-                            modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .width(150.dp)
+        // Card do Paciente
+        PatientCard()
 
-                ) {
-                    Text(
-                        text = "Médico",
-                        fontSize = 20.sp,
-                        color = Color.White
-                    )
-                }
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(AzulMedio),
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .width(150.dp)
-                ) {
-                    Text(
-                        text = "Paciente",
-                        fontSize = 20.sp,
-                        color = Color.White
-                    )
-                }
+        // Botões de navegação
+        Column(
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(vertical = 16.dp)
+        ) {
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(AzulMedio),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .width(150.dp)
+            ) {
+                Text(
+                    text = "Médico",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
             }
-            Spacer(modifier = Modifier.height(40.dp))
-            Row (modifier = Modifier.background(Color.Transparent)){
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(AzulMedio),
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .width(150.dp)
-
-                ) {
-                    Text(
-                        text = "Prontuário",
-                        fontSize = 20.sp,
-                        color = Color.White
-                    )
-                }
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(AzulMedio),
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .width(150.dp)
-                ) {
-                    Text(
-                        text = "Consulta",
-                        fontSize = 20.sp,
-                        color = Color.White
-                    )
-                }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(AzulMedio),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .width(150.dp)
+            ) {
+                Text(
+                    text = "Paciente",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
             }
-
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(AzulMedio),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .width(150.dp)
+            ) {
+                Text(
+                    text = "Prontuário",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(AzulMedio),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .width(150.dp)
+            ) {
+                Text(
+                    text = "Consulta",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
+
         // Adiciona a segunda linha de ícones
         Row(
             modifier = Modifier
@@ -138,29 +129,65 @@ fun MenuScreen() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.menu_24),
+                painter = painterResource(id = R.drawable.arrow_back_ios_24),
                 contentDescription = "Icon 1",
                 Modifier.padding(10.dp)
             )
             Icon(
-                painter = painterResource(id = R.drawable.menu_24),
+                painter = painterResource(id = R.drawable.home_24),
                 contentDescription = "Icon 2",
                 Modifier.padding(10.dp)
             )
             Icon(
-                painter = painterResource(id = R.drawable.menu_24),
+                painter = painterResource(id = R.drawable.contact_phone_24),
                 contentDescription = "Icon 3",
                 Modifier.padding(10.dp)
             )
             Icon(
-                painter = painterResource(id = R.drawable.menu_24),
+                painter = painterResource(id = R.drawable.logout_24),
                 contentDescription = "Icon 4",
                 Modifier.padding(10.dp)
             )
         }
     }
+}
 
-
+@Composable
+fun PatientCard() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Brush.linearGradient(colors = listOf(AzulMedio, AzulPiscina)))
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.person_24),
+            contentDescription = "Ícone de Paciente",
+            modifier = Modifier.size(48.dp)
+        )
+        Column(
+            modifier = Modifier.padding(start = 16.dp)
+        ) {
+            Text(
+                text = "Nome do Paciente",
+                fontSize = 18.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Data de Nascimento: 01/01/2000",
+                fontSize = 14.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "CPF: 123.456.789-00",
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
